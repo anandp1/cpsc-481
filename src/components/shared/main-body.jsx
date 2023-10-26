82d23d295454dc21ce68b3f32b7df8ed29bb55d7
Anand Patel <73033746+anandp1@users.noreply.github.com>
2023-10-26 14:55:09 -0600

    finished landing page (#3)

--- src/components/shared/main-body.jsx
+++ src/components/shared/main-body.jsx
@@ -7,10 +7,17 @@ const MainBody = ({ children }) => {
     <div className="bg-gray-200 bg-opacity-80">
       <div className="flex flex-row min-h-screen max-h-screen">
         <div className="flex flex-col w-3/4">
+          <div className="flex flex-row">
+            <div className="w-1/2 text-center bg-gray-300 py-2 cursor-pointer rounded-tl-md">
+              <p className="text-xl font-bold text-gray-700">Concessions</p>
+            </div>
+            <div className="w-1/2 text-center bg-gray-200 py-2 cursor-pointer rounded-tr-md">
+              <p className="text-xl font-bold text-gray-700">Tickets</p>
+            </div>
+          </div>
           {children}
           <Navigation />
         </div>
-
         <SideCart />
       </div>
     </div>
