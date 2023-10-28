58eb2ecbe1412f32d26678c6c8bec9b5796e5458
Anand Patel <anandpp2001@gmail.com>
2023-10-28 14:15:48 -0600

    fixed landing

--- src/components/shared/layout.jsx
+++ src/components/shared/layout.jsx
@@ -1,12 +1,12 @@
 import HeadComponent from "./head-component";
 import MainBody from "./main-body";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, isLanding }) => {
   return (
     <>
       <HeadComponent />
       <div className="w-full">
-        <MainBody>{children}</MainBody>
+        <MainBody isLanding={isLanding}>{children}</MainBody>
       </div>
     </>
   );
