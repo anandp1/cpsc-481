6f1db3473bfb0123b1f9b43c4ee4dd2a9d461a3f
Jacob Nguyen <jacobn0223@outlook.com>
2023-11-02 20:52:43 -0600

    Fix z-index of navigation buttons

--- src/components/shared/navigation.jsx
+++ src/components/shared/navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = () => {
           handleClose={handleClose}
         />
       )}
-      <div className="absolute bottom-0 left-0">
+      <div className="absolute bottom-0 left-0 z-1500">
         <div className="flex bg-gray-100 border border-gray-400/80 shadow-md rounded-tr-lg p-4 w-40 h-14">
           <div className="flex flex-row items-center">
             <ArrowLeftOnRectangleIcon className="w-6 h-6 text-gray-900" />
@@ -29,15 +29,14 @@ const Navigation = () => {
           />
         </div>
       </div>
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-1500">
         <Link href="/" draggable={false} >
           <div className="rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-pink-500 hover:to-yellow-500 shadow-md border-4 border-white p-3 mb-2 hover:cursor-pointer">
             <HomeIcon className="w-8 h-8 text-white" />
           </div>
         </Link>
-
       </div>
-      <div className="absolute bottom-0 right-0">
+      <div className="absolute bottom-0 right-0 z-1500">
         <div className="flex items-center justify-center bg-gray-100 border border-gray-400/80 shadow-md rounded-tl-lg p-4 w-40 h-14 hover:bg-gray-200 hover:cursor-pointer">
           <SubdirectoryArrowLeftIcon className="w-8 h-8 text-gray-900" />
         </div>
